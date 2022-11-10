537f6bd08c80f85b04ff863be03fd288753105f5
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2022-11-11 01:56:49 +0900

    Update SongActivity.kt

--- app/src/main/java/com/example/flo/SongActivity.kt
+++ app/src/main/java/com/example/flo/SongActivity.kt
@@ -1,6 +1,7 @@
 package com.example.flo
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flo.databinding.ActivitySongBinding
 
@@ -12,7 +13,30 @@ class SongActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySongBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.songDownIb.setOnClickListener{
+            finish()
+        }
+
+        binding.songMiniplayerIv.setOnClickListener {
+            setPlayerStatus(false)
+        }
+        binding.songPauseIv.setOnClickListener{
+            setPlayerStatus(true)
+        }
     }
+
+    fun setPlayerStatus(isPlaying: Boolean){
+        if(isPlaying){
+            binding.songMiniplayerIv.visibility = View.VISIBLE
+            binding.songPauseIv.visibility = View.GONE
+        }else{
+            binding.songMiniplayerIv.visibility = View.GONE
+            binding.songPauseIv.visibility = View.VISIBLE
+        }
+    }
+
+
 
 
 }
