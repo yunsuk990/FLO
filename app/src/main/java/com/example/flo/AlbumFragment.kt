73594a3a7dcb80b136ed271d93944fd23614a43b
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2022-11-12 23:52:58 +0900

    Create AlbumFragment

---
+++ app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -0,0 +1,26 @@
+package com.example.flo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.flo.databinding.FragmentAlbumBinding
+
+class AlbumFragment: Fragment() {
+
+    lateinit var binding: FragmentAlbumBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAlbumBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+
+
+}
