3e52242d940b7e71b9321d9a153219def64bdd9a
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2022-11-17 12:58:16 +0900

    Add ViewPager2

--- app/src/main/java/com/example/flo/homeFragment.kt
+++ app/src/main/java/com/example/flo/homeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.example.flo.databinding.FragmentHomeBinding
 
 
@@ -24,6 +25,12 @@ class homeFragment : Fragment() {
                 .replace(R.id.main_frm, AlbumFragment())
                 .commitAllowingStateLoss()
         }
+
+        val bannerAdapter = BannerVPAdapter(this)
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
+        bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp2))
+        binding.homePopAcousticBanner.adapter = bannerAdapter
+        binding.homePopAcousticBanner.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         return binding.root
     }
 
