b78ee43db68387928e5bb9a41a5f7a24384c798b
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2022-11-17 23:02:47 +0900

    Add TabLayout_ViewPager2

--- app/src/main/java/com/example/flo/AlbumFragment.kt
+++ app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentAlbumBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class AlbumFragment: Fragment() {
 
     lateinit var binding: FragmentAlbumBinding
     var checkAlbumIpIv = false
+    private val information = arrayListOf("수록곡", "상세정보", "영상")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,6 +36,15 @@ class AlbumFragment: Fragment() {
                 checkAlbumIpIv = true
             }
         }
+
+        val albumAdapter = AlbumVPAdapter(this)
+        binding.albumContentVp.adapter = albumAdapter
+        TabLayoutMediator(binding.albumTabLayout, binding.albumContentVp){
+            tab, position ->
+            tab.text = information[position]
+        }.attach()
+
+
         return binding.root
     }
 
