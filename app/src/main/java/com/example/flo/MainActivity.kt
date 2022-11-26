e4a6bff2ac93f16afb930514e1123b967b6431e6
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2022-11-27 01:40:33 +0900

    Use SharedPreferences, Gson

--- app/src/main/java/com/example/flo/MainActivity.kt
+++ app/src/main/java/com/example/flo/MainActivity.kt
@@ -4,10 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.flo.databinding.ActivityMainBinding
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private var song: Song = Song()
+    private var gson: Gson = Gson()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         initBottomNavigation()
 
         //음악 초기화
-        val song = Song(binding.mainSongTitleTv.text.toString(), binding.mainSongSingerTv.text.toString(), 0, 60, false, "music_lilac")
+        //val song = Song(binding.mainSongTitleTv.text.toString(), binding.mainSongSingerTv.text.toString(), 0, 60, false, "music_lilac")
 
         binding.mainPlayerCl.setOnClickListener{
             val intent = Intent(this, SongActivity::class.java)
@@ -70,5 +73,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setMiniPlayer(song: Song){
+        binding.mainSongTitleTv.text = song.title
+        binding.mainSongSingerTv.text = song.singer
+        binding.songProgressbarView.progress = (song.second*100000)/song.playTime
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val sharedPreferences = getSharedPreferences("song", MODE_PRIVATE)
+        val songJson = sharedPreferences.getString("song", null)
+        song = if(songJson == null){
+            Song("라일릭", "아이유(IU)", 0, 60, false, "music_lilac")
+        }else{
+            gson.fromJson(songJson, Song::class.java) //songJson의 Json형식을 Song클래스 자바 객체로 변환
+        }
+        setMiniPlayer(song)
+    }
 
 }
