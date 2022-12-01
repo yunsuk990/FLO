1b21aa562a68a38df8f85de11155e3dda8cb4ffa
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2022-12-01 22:55:21 +0900

    Add Album_Data

--- app/src/main/java/com/example/flo/homeFragment.kt
+++ app/src/main/java/com/example/flo/homeFragment.kt
@@ -12,6 +12,7 @@ import com.example.flo.databinding.FragmentHomeBinding
 class homeFragment : Fragment() {
 
     lateinit var binding: FragmentHomeBinding
+    private var albumDatas = ArrayList<Album>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,11 +21,16 @@ class homeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
 
-        binding.homeAlbumImageIv.setOnClickListener {
-            (context as MainActivity).supportFragmentManager.beginTransaction()
-                .replace(R.id.main_frm, AlbumFragment())
-                .commitAllowingStateLoss()
+        albumDatas.apply {
+            add(Album("Butter", "방탄소년단(BTS)", R.drawable.img_album_exp))
+            add(Album("Lilac", "아이유(IU)", R.drawable.img_album_exp2))
+            add(Album("Next Level", "에스파(AESPA)", R.drawable.img_album_exp3))
+            add(Album("Boy with Luv", "방탄소년단(BTS)", R.drawable.img_album_exp4))
+            add(Album("BBoom BBoom", "모모랜드(MOMOLAND)", R.drawable.img_album_exp5))
+            add(Album("Weekend", "태연(Tae Yeon)", R.drawable.img_album_exp6))
         }
+
+
 
         val bannerAdapter = BannerVPAdapter(this)
         bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
