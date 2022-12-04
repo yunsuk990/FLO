bd0cec2ff1a1d340effacd71b5f33db4178e2160
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2022-12-04 19:24:28 +0900

    Update homeFragment.kt

--- app/src/main/java/com/example/flo/homeFragment.kt
+++ app/src/main/java/com/example/flo/homeFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.flo.databinding.FragmentHomeBinding
+import com.google.gson.Gson
 
 
 class homeFragment : Fragment() {
@@ -36,10 +37,11 @@ class homeFragment : Fragment() {
         binding.homeTodayMusicAlbumRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
         albumAdapter.setMyItemClickListener(object: AlbumRVAdapter.MyItemClickOnListener{
-            override fun onItemClick() {
-                (context as MainActivity).supportFragmentManager.beginTransaction()
-                    .replace(R.id.main_frm, AlbumFragment())
-                    .commitAllowingStateLoss()
+            override fun onItemClick(album: Album) {
+                changeAlbumFragment(album)
+            }
+            override fun onRemoveAlbum(position: Int) {
+                albumAdapter.deleteItem(position)
             }
         })
 
@@ -49,6 +51,18 @@ class homeFragment : Fragment() {
         binding.homePopAcousticBanner.adapter = bannerAdapter
         binding.homePopAcousticBanner.orientation = ViewPager2.ORIENTATION_HORIZONTAL
         return binding.root
+    }
+
+    private fun changeAlbumFragment(album: Album) {
+        (context as MainActivity).supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, AlbumFragment().apply {
+                arguments = Bundle().apply {
+                    val gson = Gson()
+                    val albumJson = gson.toJson(album)
+                    putString("album", albumJson)
+                }
+            })
+            .commitAllowingStateLoss()
     }
 
 }
