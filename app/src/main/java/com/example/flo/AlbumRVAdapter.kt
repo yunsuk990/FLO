3a75b9802d1197a5630527c60c2867266279d466
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2022-12-04 19:24:24 +0900

    Update AlbumRVAdapter.kt

--- app/src/main/java/com/example/flo/AlbumRVAdapter.kt
+++ app/src/main/java/com/example/flo/AlbumRVAdapter.kt
@@ -8,12 +8,23 @@ import com.example.flo.databinding.ItemAlbumBinding
 class AlbumRVAdapter(private val albumList: ArrayList<Album>): RecyclerView.Adapter<AlbumRVAdapter.ViewHolder>() {
 
     interface MyItemClickOnListener{
-        fun onItemClick()
+        fun onItemClick(album: Album)
+        fun onRemoveAlbum(position: Int)
     }
 
     private lateinit var mItemClickListener: MyItemClickOnListener
     fun setMyItemClickListener(itemClickListener: MyItemClickOnListener){
         mItemClickListener = itemClickListener
+    }
+
+    fun addItem(album: Album){
+        albumList.add(album)
+        notifyDataSetChanged()
+    }
+
+    fun deleteItem(position: Int){
+        albumList.removeAt(position)
+        notifyDataSetChanged()
     }
 
     inner class ViewHolder(val binding: ItemAlbumBinding): RecyclerView.ViewHolder(binding.root) {
@@ -31,7 +42,8 @@ class AlbumRVAdapter(private val albumList: ArrayList<Album>): RecyclerView.Adap
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bind(albumList[position])
-        holder.itemView.setOnClickListener{mItemClickListener.onItemClick()}
+        holder.itemView.setOnClickListener{mItemClickListener.onItemClick(albumList[position])}
+        //holder.binding.itemAlbumTitleTv.setOnClickListener { mItemClickListener.onRemoveAlbum(position)}
     }
 
     override fun getItemCount(): Int = albumList.size
