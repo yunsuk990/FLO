e0a0dbe6abe06682b961c1868f87f327be956250
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2022-12-04 19:01:51 +0900

    Connect RecyclerView-Adapter

--- app/src/main/java/com/example/flo/homeFragment.kt
+++ app/src/main/java/com/example/flo/homeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.flo.databinding.FragmentHomeBinding
 
@@ -30,7 +31,17 @@ class homeFragment : Fragment() {
             add(Album("Weekend", "태연(Tae Yeon)", R.drawable.img_album_exp6))
         }
 
+        val albumAdapter = AlbumRVAdapter(albumDatas)
+        binding.homeTodayMusicAlbumRv.adapter = albumAdapter
+        binding.homeTodayMusicAlbumRv.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
+        albumAdapter.setMyItemClickListener(object: AlbumRVAdapter.MyItemClickOnListener{
+            override fun onItemClick() {
+                (context as MainActivity).supportFragmentManager.beginTransaction()
+                    .replace(R.id.main_frm, AlbumFragment())
+                    .commitAllowingStateLoss()
+            }
+        })
 
         val bannerAdapter = BannerVPAdapter(this)
         bannerAdapter.addFragment(BannerFragment(R.drawable.img_home_viewpager_exp))
