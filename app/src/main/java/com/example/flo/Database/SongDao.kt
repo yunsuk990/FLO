c3fdc213952084fb9c9e48b2fea47dd0469ede21
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2022-12-06 00:30:15 +0900

    Create Database

---
+++ app/src/main/java/com/example/flo/Database/SongDao.kt
@@ -0,0 +1,22 @@
+package com.example.flo.Database
+
+import androidx.room.*
+
+@Dao
+interface SongDao {
+
+    @Insert
+    fun insert(song: Song)
+
+    @Update
+    fun update(song: Song)
+
+    @Delete
+    fun delete(song: Song)
+
+    @Query("SELECT * FROM SongTable")
+    fun getSongs(): List<Song>
+
+    @Query("SELECT * FROM SongTable WHERE id = :id")
+    fun getSong(id: Int): Song
+}
