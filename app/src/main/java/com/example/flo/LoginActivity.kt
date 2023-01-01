10d6ebbaefd040f2adf9424f969655e5da2a93c7
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-01 21:32:31 +0900

    Add LoginService

--- app/src/main/java/com/example/flo/LoginActivity.kt
+++ app/src/main/java/com/example/flo/LoginActivity.kt
@@ -3,6 +3,10 @@ package com.example.flo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.flo.Database.SongDatabase
+import com.example.flo.Database.User
 import com.example.flo.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -17,6 +21,47 @@ class LoginActivity : AppCompatActivity() {
         binding.loginRegisterTv.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
+
+        binding.loginLoginBtn.setOnClickListener {
+            login()
+        }
+    }
+
+    private fun login(){
+        if(binding.loginIdEt.text.toString().isEmpty() || binding.loginDirectInputEt.text.toString().isEmpty()){
+            Toast.makeText(this, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(binding.loginPasswordEt.text.toString().isEmpty()){
+            Toast.makeText(this, "비밀번호를 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        var email = binding.loginIdEt.text.toString() + "@" + binding.loginDirectInputEt.text.toString()
+        var password = binding.loginPasswordEt.text.toString()
+
+        val songDB = SongDatabase.getInstance(this)!!
+
+        val user = songDB.UserDao().getUser(email, password)
+        if(user != null){
+            Log.d("LOGIN_ACT/GET_USER", "userId : ${user.id}, $user")
+            saveJwt(user.id)
+            startMainActivity()
+        }else{
+            Toast.makeText(this, "회원정보가 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun saveJwt(jwt: Int){
+        val spf = getSharedPreferences("auth", MODE_PRIVATE)
+        val editor = spf.edit()
+        editor.putInt("jwt", jwt)
+        editor.apply()
+    }
+
+    private fun startMainActivity(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 
 }
