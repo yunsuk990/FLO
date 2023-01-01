a11f8d2a9744e7131b2c0794d90ef95716597e41
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-01 21:32:41 +0900

    Add LogOutService

--- app/src/main/java/com/example/flo/lockerFragment.kt
+++ app/src/main/java/com/example/flo/lockerFragment.kt
@@ -2,9 +2,11 @@ package com.example.flo
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Display.Mode
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentLockerBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -34,6 +36,40 @@ class lockerFragment : Fragment() {
         }
 
         return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initViews()
+    }
+
+    private fun getJwt(): Int{
+        val spf = activity?.getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
+        return spf!!.getInt("jwt", 0)
+    }
+
+    private fun initViews(){
+        val jwt: Int= getJwt()
+        if(jwt == 0){
+            binding.login.text = "로그인"
+            binding.login.setOnClickListener {
+                startActivity(Intent(activity, LoginActivity::class.java))
+            }
+        }else{
+            binding.login.text = "로그아웃"
+            binding.login.setOnClickListener {
+                //로그아웃 진행
+                logout()
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+        }
+    }
+
+    private fun logout(){
+        val spf = activity?.getSharedPreferences("auth", AppCompatActivity.MODE_PRIVATE)
+        val editor = spf!!.edit()
+        editor.remove("jwt")
+        editor.apply()
     }
 
 
