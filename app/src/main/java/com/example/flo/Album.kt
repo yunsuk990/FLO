2545971c7d4ef162fb21882577bb4be3036ed246
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-01 23:48:27 +0900

    Update Album.kt

--- app/src/main/java/com/example/flo/Album.kt
+++ app/src/main/java/com/example/flo/Album.kt
@@ -1,10 +1,13 @@
 package com.example.flo
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.flo.Database.Song
 
+@Entity(tableName = "AlbumTable")
 data class Album(
+    @PrimaryKey(autoGenerate = false) var id: Int = 0,
     var title: String? = "",
     var singer: String? = "",
-    var coverImg: Int? = null,
-    var songs: ArrayList<Song>? = null
+    var coverImg: Int? = null
 )
