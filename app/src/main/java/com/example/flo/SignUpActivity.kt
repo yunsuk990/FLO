668fc1ed74c7a32069cbd32a8150f4bea992ca74
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-01 21:05:01 +0900

    Update SignUpActivity.kt

--- app/src/main/java/com/example/flo/SignUpActivity.kt
+++ app/src/main/java/com/example/flo/SignUpActivity.kt
@@ -2,10 +2,46 @@ package com.example.flo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.flo.Database.SongDatabase
+import com.example.flo.Database.User
+import com.example.flo.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.registerSignupBtn.setOnClickListener {
+            signUp()
+        }
+    }
+
+    private fun getUser(): User {
+        val email: String = binding.loginIdEt.text.toString() + "@" + binding.loginDirectInputEt.text.toString()
+        val pwd: String = binding.loginPasswordEt.text.toString()
+        return User(email, pwd)
+    }
+
+    private fun signUp(){
+        if(binding.loginIdEt.text.toString().isEmpty() || binding.loginDirectInputEt.text.toString().isEmpty()){
+            Toast.makeText(this, "이메일 형식이 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(binding.loginPasswordEt.text.toString() !=  binding.loginCheckpasswordEt.text.toString()){
+            Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val userDB = SongDatabase.getInstance(this)!!
+        userDB.UserDao().insert(getUser())
+
+        val user = userDB.UserDao().getUsers()
+        Log.d("SIGNUPACT", user.toString())
+        finish()
     }
 }
