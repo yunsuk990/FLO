8d0b9a4988ddfe29364cb8bc3044eb8646800cb8
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-02 18:47:34 +0900

    Create RetrofitService

---
+++ app/src/main/java/com/example/flo/Network/AuthRetrofitInterface.kt
@@ -0,0 +1,16 @@
+package com.example.flo.Network
+
+import com.example.flo.Database.User
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthRetrofitInterface {
+
+    @POST("/users")
+    fun signUp(@Body user: User): Call<AuthResponse>
+
+    @POST("/users/login")
+    fun login(@Body user: User): Call<AuthResponse>
+
+}
