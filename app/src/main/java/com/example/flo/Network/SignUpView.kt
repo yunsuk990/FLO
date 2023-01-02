1bd656b8becf0b8602e501d57dd91e00b7a4ae35
yunsuk990 <45827567+yunsuk990@users.noreply.github.com>
2023-01-02 18:47:49 +0900

    Create SignUpService

---
+++ app/src/main/java/com/example/flo/Network/SignUpView.kt
@@ -0,0 +1,8 @@
+package com.example.flo.Network
+
+interface SignUpView {
+
+    fun onSignUpSuccess()
+
+    fun onSignUpFailure()
+}
--- app/src/main/java/com/example/flo/SignUpActivity.kt
+++ app/src/main/java/com/example/flo/SignUpActivity.kt
@@ -3,12 +3,17 @@ package com.example.flo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.example.flo.Database.SongDatabase
 import com.example.flo.Database.User
+import com.example.flo.Network.*
 import com.example.flo.databinding.ActivitySignUpBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity(), SignUpView {
 
     lateinit var binding: ActivitySignUpBinding
 
@@ -25,23 +30,53 @@ class SignUpActivity : AppCompatActivity() {
     private fun getUser(): User {
         val email: String = binding.loginIdEt.text.toString() + "@" + binding.loginDirectInputEt.text.toString()
         val pwd: String = binding.loginPasswordEt.text.toString()
-        return User(email, pwd)
+        val name: String = binding.loginNameEt.text.toString()
+        return User(email, pwd, name)
     }
 
-    private fun signUp(){
+//    private fun signUp(){
+//        if(binding.loginIdEt.text.toString().isEmpty() || binding.loginDirectInputEt.text.toString().isEmpty()){
+//            Toast.makeText(this, "이메일 형식이 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+//            return
+//        }
+//        if(binding.loginPasswordEt.text.toString() !=  binding.loginCheckpasswordEt.text.toString()){
+//            Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+//            return
+//        }
+//        val userDB = SongDatabase.getInstance(this)!!
+//        userDB.UserDao().insert(getUser())
+//
+//        val user = userDB.UserDao().getUsers()
+//        Log.d("SIGNUPACT", user.toString())
+//        finish()
+//    }
+
+    private fun signUp() {
         if(binding.loginIdEt.text.toString().isEmpty() || binding.loginDirectInputEt.text.toString().isEmpty()){
             Toast.makeText(this, "이메일 형식이 잘못되었습니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        if(binding.loginNameEt.text.toString().isEmpty()){
+            Toast.makeText(this, "이름 형식이 잘못되었습니다.", Toast.LENGTH_SHORT).show()
             return
         }
         if(binding.loginPasswordEt.text.toString() !=  binding.loginCheckpasswordEt.text.toString()){
             Toast.makeText(this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
             return
         }
-        val userDB = SongDatabase.getInstance(this)!!
-        userDB.UserDao().insert(getUser())
 
-        val user = userDB.UserDao().getUsers()
-        Log.d("SIGNUPACT", user.toString())
+        val authService = AuthService()
+        authService.setSignUpView(this)
+        authService.signUp(getUser())
+
+    }
+
+    override fun onSignUpFailure() {
         finish()
     }
+
+    override fun onSignUpSuccess() {
+
+    }
+
 }
